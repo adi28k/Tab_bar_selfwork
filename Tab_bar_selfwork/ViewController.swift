673b3d8a458b3e4d8b747526eb63bacb366b850c
Adilet Kistaubayev <adilet28k@gmail.com>
2025-08-30 22:57:20 +0500

    first commit

---
+++ Tab_bar_selfwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tab_bar_selfwork
+//
+//  Created by Adilet Kistaubayev on 30.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tab_bar_selfworkTests/Tab_bar_selfworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tab_bar_selfworkTests.swift
+//  Tab_bar_selfworkTests
+//
+//  Created by Adilet Kistaubayev on 30.08.2025.
+//
+
+import Testing
+@testable import Tab_bar_selfwork
+
+struct Tab_bar_selfworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
