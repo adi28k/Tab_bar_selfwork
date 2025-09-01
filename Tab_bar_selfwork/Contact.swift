bdcb2d67113a8c7a004e5b64533138661157507c
Adilet Kistaubayev <adilet28k@gmail.com>
2025-09-01 15:34:44 +0500

    tab_bar_selfwork

---
+++ Tab_bar_selfwork/Contact.swift
@@ -0,0 +1,16 @@
+//
+//  Contact.swift
+//  Tab_bar_selfwork
+//
+//  Created by Adilet Kistaubayev on 31.08.2025.
+//
+
+struct Contact: Codable  {
+    
+    var name : String
+    
+    var surname : String
+    
+    var phoneNumber : String
+}
+
--- Tab_bar_selfwork/ViewController.swift
+++ Tab_bar_selfwork/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var surnameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func addContact(_ sender: Any) {
+        
+        let name = nameTextField.text
+        
+        let surname = surnameTextField.text
+        
+        let phoneNumber = phoneNumberTextField.text
+        
+        
+        
+    }
+    
 
 }
 
